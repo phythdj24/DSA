fc24ff63bbcf8e2b221f266cf4c95a8180834c1d
ATHARV DALAL <sangamdalal24@gmail.com>
2023-12-22 15:27:06 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,14 @@
+//problem statement -1
+// checking sum zero [-5,-4,-2,0,2,4,6,8]
+
+function getSumPairZero(array){
+    for(let number of array){
+      for(let j=1; j<array.length; j++){
+        if(number + array[j] === 0){
+            return [number,array[j]];
+        }
+      }
+    }
+}
+const result = getSumPairZero([-5,-4,-2,0,2,4,6,8])
+console.log(result)
